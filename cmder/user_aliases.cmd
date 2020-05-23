7f94661446ae6f55e372013748269a1dc113bc8c
rpujan <rpujan@insightresults.com>
2020-05-23 08:33:50 +0530

    initial version

---
+++ cmder/user_aliases.cmd
@@ -0,0 +1,47 @@
+;= rem Path of file: cmder\config
+
+;= @echo off
+;= rem Call DOSKEY and use this file as the macrofile
+;= %SystemRoot%\system32\doskey /listsize=1000 /macrofile=%0%
+;= rem In batch mode, jump to the end of the file
+;= goto:eof
+;= Add aliases below here
+e.=explorer .
+gl=git log --oneline --all --graph --decorate  $*
+ls=ls --show-control-chars -F --color $*
+pwd=cd
+clear=cls
+history=cat -n "%CMDER_ROOT%\config\.history"
+unalias=alias /d $1
+vi=vim $*
+cmderr=cd /d "%CMDER_ROOT%"
+
+..=cd ..
+...=cd ../..
+....=cd ../../..
+....=cd ../../../..
+work=cd c:/work
+projects=cd c:/work/projects
+insight=cd c:/work/projects/insight
+dm=cd c:/work/projects/insight/analytics/datamanager/react/datamanager
+meeting=cd c:/work/projects/insight/brainstormod/meeting/react/meeting
+vici=cd c:/work/projects/vici/viciproject/app/react/ideas/src
+~=cd %homepath%
+
+;= rem The rest of my git aliases
+gl=git pull --prune
+
+glog=git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative
+gp=git push origin HEAD
+
+;=rem Remove `+` and `-` from start of diff lines; just rely upon color.
+gd=git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r
+
+gc=git commit
+gca=git commit -a
+gco=git checkout
+gcb=git copy-branch-name
+gb=git branch
+gs=git status -sb
+gac=git add -A && git commit -m
+ge=git-edit-new
